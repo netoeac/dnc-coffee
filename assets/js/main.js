a95396d2143668cc16b497899a0533a124614091
netoeac <netoeac.dev@gmail.com>
2023-01-21 20:04:29 -0300

    infoModal added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 function showButton(selector) {
   var buyButton = document. querySelector(selector)
-  //var x = document.getElementById("button-div");
   if (buyButton.style.display === "block") {
     buyButton.style.display = "none";
   } else {
     buyButton.style.display = "block";
   }
+}
+
+
+function showModal(modalName){
+  let modal = document.getElementById(modalName);
+  modal.style.display = 'block'
+
+}
+
+function hideModal(modalName){
+  let modal = document.getElementById(modalName);
+  modal.style.display = 'none'
 }
